2e67519e7a7ec1b904da13dc5efaab3841708646
Felipe Bachetti <84641892+FelipeBachetti@users.noreply.github.com>
2023-11-15 14:27:59 -0300

    table jogadores

---
+++ database_connection.py
@@ -0,0 +1,42 @@
+import psycopg2 as psy
+
+def init():
+    conn = psy.connect(database = "Futnotas", host = "localhost", user = "postgres", password = "1234", port = "5432")
+    cur = conn.cursor()
+    
+    if(conn.status==1):
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS Jogadores (
+                        id SERIAL PRIMARY KEY,
+                        nome VARCHAR(50),  
+                        clube VARCHAR(50),
+                        gols SMALLINT,
+                        assists SMALLINT,
+                        passescertos SMALLINT,
+                        passeserrados SMALLINT,
+                        passesimportantes SMALLINT,
+                        chutesnogol SMALLINT,
+                        chutesprafora SMALLINT,
+                        chutesbloqueados SMALLINT,
+                        driblescertos SMALLINT,
+                        dribleserrados SMALLINT,
+                        driblessofridos SMALLINT,
+                        duelosganhos SMALLINT,
+                        duelosperdidos SMALLINT,
+                        posseperdida SMALLINT,
+                        faltascometidas SMALLINT,
+                        faltassofridas SMALLINT,
+                        interceptacoes SMALLINT,
+                        desarmes SMALLINT                  
+                    );
+                    """)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    else:
+        cur.close()
+        conn.close()
+        return False
+    
+init()
